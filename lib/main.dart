24ea3a4a72347cc7c04ec5bdefb84bd8738c8695
aamorim <aamorim@ciandt.com>
2023-07-21 17:24:53 -0300

    Aulas 101 - Usando Textfield 02 - Adicionando TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -90,11 +93,13 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(
                       labelText: "Titulo",
                     ),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(
                       labelText: "Valor (R\$)",
                     ),
@@ -103,7 +108,10 @@ class MyHomePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       TextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                         child: Text(
                           'Nova Transação',
                           style: TextStyle(
